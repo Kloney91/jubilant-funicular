7736a98a2798337884420092f5d32ed918fe3316
Kyle Loney <kyleloney9109@gmail.com>
2022-04-05 20:44:22 -0500

    weather report

---
+++ src/main.js
@@ -0,0 +1,10 @@
+const mytext="THE WEATHER IS CLOUDY"
+const lower=mytext.toLowerCase();
+console.log(lower);
+const original="Stormy weather";
+const replace=original.replace("Stormy","Cold");
+console.log(replace);
+const x=4
+const y=4
+const sum=`4+4=${x + y}`
+console.log(sum)
